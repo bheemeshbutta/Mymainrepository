edf7018b976ff9cabd5c3b78b00ca533d2e8e864
Bheemesh <Bheemesh@Bheemesh>
2018-07-26 22:42:18 +0530

    Third program

---
+++ tGit/src/tPackage/Sample.java
@@ -0,0 +1,19 @@
+package tPackage;
+
+public class Sample {
+	void display()
+	{
+		System.out.println("hi bheemesh");
+	}
+ void display1() {
+	 System.out.println("hi bheesh how are you");
+ }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sample obj=new Sample();
+		obj.display();
+		obj.display1();
+
+	}
+
+}
