a1f05d43c4586438abd035becdccc68455b167d1
Bheemesh <Bheemesh@Bheemesh.local>
2018-07-26 20:54:00 +0530

    This is first program

---
+++ tGit/src/tPackage/Myfirstpro.java
@@ -0,0 +1,11 @@
+package tPackage;
+
+public class Myfirstpro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to git");
+
+	}
+
+}
