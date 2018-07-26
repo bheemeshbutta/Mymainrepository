df8073d4c16828927e613c4034cb439a5ca43e2d
Bheemesh <Bheemesh@Bheemesh.local>
2018-07-26 21:00:41 +0530

    bheemesh second program

---
+++ tGit/src/tPackage/Testing.java
@@ -0,0 +1,11 @@
+package tPackage;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to git this is second pro");
+
+	}
+
+}
